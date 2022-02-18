e0884f1b9f6da69b9e6f54f7cb9460940f8b3a5f
Tirta Abimanyu <tirtaabimanyu@gmail.com>
2022-02-18 17:43:08 +0700

    numbers is now endless

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,14 @@ import cn from "classnames";
 import { useState, useEffect, useReducer, useRef } from "react";
 import styles from "../styles/Home.module.css";
 
+const allowedKeys = new Set([
+  8, 13, 38, 40, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100,
+  101, 102, 103, 104, 105,
+]);
+
 const ACTIONS = {
+  INIT_NUMBERS: "init-numbers",
+  GENERATE_NEXT_NUMBERS: "generate-next-numbers",
   PUSH_ANSWER: "push-answer",
   INCREMENT: "increment",
   DECREMENT: "decrement",
@@ -12,48 +19,45 @@ const ACTIONS = {
   DECREASE_TIME: "decrease-time",
 };
 
-export async function getServerSideProps() {
-  return {
-    props: {
-      numbers: Array.from({ length: 25 }, () => Math.floor(Math.random() * 10)),
-    },
-  };
-}
-
-function useInterval(callback, delay) {
-  const savedCallback = useRef();
-
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
-}
-
 function reducer(state, action) {
   switch (action.type) {
+    case ACTIONS.INIT_NUMBERS:
+      const numbers = Array.from({ length: 100 }, () =>
+        Math.floor(Math.random() * 10)
+      );
+      return {
+        ...state,
+        numbers,
+        answers: Array(numbers.length).fill(null),
+      };
+    case ACTIONS.GENERATE_NEXT_NUMBERS:
+      console.log("hehe");
+      const newNumbers = Array.from({ length: 100 }, () =>
+        Math.floor(Math.random() * 10)
+      );
+      const newAnswers = Array(state.numbers.length + newNumbers.length).fill(
+        null
+      );
+      return {
+        ...state,
+        numbers: state.numbers.concat(newNumbers),
+        answers: state.answers.concat(newAnswers),
+      };
+
     case ACTIONS.INCREMENT:
       return {
         ...state,
-        active: Math.min(state.numbersLength - 1, state.active + 1),
+        active: Math.min(state.maxActive, state.active + 1),
       };
     case ACTIONS.DECREMENT:
       return { ...state, active: Math.max(0, state.active - 1) };
     case ACTIONS.PUSH_ANSWER:
       state.answers[state.active] = action.payload.input;
+      const active = Math.min(state.numbers.length - 1, state.active + 1);
       return {
         ...state,
-        active: Math.min(state.numbersLength - 1, state.active + 1),
+        active,
+        maxActive: active,
       };
     case ACTIONS.START_GAME:
       console.log("triggered");
@@ -76,10 +80,25 @@ function reducer(state, action) {
   }
 }
 
-const allowedKeys = new Set([
-  8, 13, 38, 40, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100,
-  101, 102, 103, 104, 105,
-]);
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 function formatTime(time) {
   const seconds = (time % 60).toLocaleString("en-US", {
@@ -93,14 +112,24 @@ function formatTime(time) {
   return minute + ":" + seconds;
 }
 
-export default function Home({ numbers }) {
+export default function Home() {
   const [state, dispatch] = useReducer(reducer, {
     active: 0,
-    numbersLength: numbers.length,
-    answers: Array(numbers.length).fill(null),
+    maxActive: 0,
+    numbers: [],
+    answers: [],
     isGameRunning: false,
     secondsRemaining: 60,
   });
+
+  const stateRef = useRef(state);
+  useEffect(() => {
+    stateRef.current = state;
+  }, [state]);
+
+  useEffect(() => {
+    dispatch({ type: ACTIONS.INIT_NUMBERS });
+  }, []);
 
   useInterval(
     () => {
@@ -112,11 +141,6 @@ export default function Home({ numbers }) {
     },
     state.isGameRunning ? 1000 : null
   );
-
-  const stateRef = useRef(state);
-  useEffect(() => {
-    stateRef.current = state;
-  }, [state]);
 
   useEffect(() => {
     console.log("init");
@@ -139,17 +163,23 @@ export default function Home({ numbers }) {
 
     if (e.repeat || e.ctrlKey) return;
 
-    if (e.keyCode >= 48 && e.keyCode <= 57)
+    if (e.keyCode >= 48 && e.keyCode <= 57) {
+      if (stateRef.current.numbers.length - 10 <= stateRef.current.active)
+        dispatch({ type: ACTIONS.GENERATE_NEXT_NUMBERS });
       return dispatch({
         type: ACTIONS.PUSH_ANSWER,
         payload: { input: e.keyCode - 48 },
       });
+    }
 
-    if (e.keyCode >= 96 && e.keyCode <= 105)
+    if (e.keyCode >= 96 && e.keyCode <= 105) {
+      if (stateRef.current.numbers.length - 10 <= stateRef.current.active)
+        dispatch({ type: ACTIONS.GENERATE_NEXT_NUMBERS });
       return dispatch({
         type: ACTIONS.PUSH_ANSWER,
         payload: { input: e.keyCode - 96 },
       });
+    }
   }
 
   function createPaddingNumbers(paddingLength, active, keyPrefix) {
@@ -161,7 +191,7 @@ export default function Home({ numbers }) {
     ));
   }
 
-  const renderedNumbers = numbers.slice(
+  const renderedNumbers = state.numbers.slice(
     Math.max(0, state.active - 2),
     state.active + 4
   );
@@ -203,7 +233,7 @@ export default function Home({ numbers }) {
             })}
             {createPaddingNumbers(
               3,
-              state.numbersLength - state.active - 1,
+              state.numbers.length - state.active - 1,
               "end"
             )}
           </div>
@@ -250,7 +280,7 @@ export default function Home({ numbers }) {
               })}
             {createPaddingNumbers(
               3,
-              state.numbersLength - state.active - 1,
+              state.numbers.length - state.active - 1,
               "end"
             )}
           </div>
