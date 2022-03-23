7b27df24e1195d79df2505a9a44828cf9c3fa714
Tirta Abimanyu <tirtaabimanyu@gmail.com>
2022-03-23 18:43:12 +0700

    adjust navbar for mobile

--- components/navbar.js
+++ components/navbar.js
@@ -14,42 +14,43 @@ export default function Navbar({ hide, titleClickHandler }) {
 
   return (
     <div className={cn([styles.container])}>
-      <div className={styles.group}>
-        <div
-          className={styles.titleContainer}
-          onClick={() => {
-            if (router.pathname == "/") titleClickHandler.click();
-            else router.push("/");
-          }}
-        >
-          <h1 className={styles.title}>
-            <FaRegPlusSquare size="32px" />
-            tesKoran.id
-          </h1>
+      <div
+        className={cn([styles.titleContainer], { [styles.greyed]: hide })}
+        onClick={() => {
+          if (router.pathname == "/") titleClickHandler.click();
+          else router.push("/");
+        }}
+      >
+        <h1 className={styles.title}>
+          <FaRegPlusSquare size="32px" />
+          tesKoran.id
+        </h1>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.group}>
+          <Link href="/leaderboard">
+            <div className={cn([styles.expandable], { [styles.hide]: hide })}>
+              <FaTrophy className={styles.icon} size="24px" />
+              <h4 className={cn([styles.text])}>Leaderboard</h4>
+            </div>
+          </Link>
+
+          <Link href="/about">
+            <div className={cn([styles.expandable], { [styles.hide]: hide })}>
+              <FaInfoCircle className={styles.icon} size="24px" />
+              <h4 className={cn([styles.text])}>About</h4>
+            </div>
+          </Link>
         </div>
 
-        <Link href="/leaderboard">
-          <div className={cn([styles.expandable], { [styles.hide]: hide })}>
-            <FaTrophy className={styles.icon} size="24px" />
-            <h4 className={cn([styles.text])}>Leaderboard</h4>
-          </div>
-        </Link>
-
-        <Link href="/about">
-          <div className={cn([styles.expandable], { [styles.hide]: hide })}>
-            <FaInfoCircle className={styles.icon} size="24px" />
-            <h4 className={cn([styles.text])}>About</h4>
-          </div>
-        </Link>
-      </div>
-
-      <div className={styles.group}>
-        <Link href="/login">
-          <div className={cn([styles.loginButton], { [styles.hide]: hide })}>
-            <h4 className={cn([styles.text])}>Log In</h4>
-            <FaUserAlt className={styles.icon} size="24px" />
-          </div>
-        </Link>
+        <div className={styles.group}>
+          <Link href="/login">
+            <div className={cn([styles.loginButton], { [styles.hide]: hide })}>
+              <h4 className={cn([styles.text])}>Log In</h4>
+              <FaUserAlt className={styles.icon} size="24px" />
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
