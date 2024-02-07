577c11139d8374c35f23a3e13253e89e21d01552
tirtaabimanyu <tirtaabimanyu@gmail.com>
2024-02-07 20:48:13 +0700

    adjust page title according to pathname

--- apps/frontend/components/layout.js
+++ apps/frontend/components/layout.js
@@ -4,8 +4,28 @@ import Navbar from "./navbar.js";
 import Footer from "./footer.js";
 import styles from "./layout.module.css";
 import { ToastContainer } from "react-toastify";
+import { useRouter } from "next/router.js";
+
+const pages = {
+  "/": "tesKoran.id - Pauli & Kraepelin Test Online",
+  "/leaderboard": "Leaderboard - tesKoran.id",
+  "/about": "About - tesKoran.id",
+  "/login": "Login - tesKoran.id",
+  "/profile": "Profile - tesKoran.id",
+  "/contact": "Contact Us - tesKoran.id",
+  "/terms": "Terms of Service - tesKoran.id",
+  "/privacy": "Privacy Policy - tesKoran.id",
+  "/activation": "Email Verification - tesKoran.id",
+  "/password-reset": "Reset Password - tesKoran.id",
+};
 
 export default function Layout({ children, hide, titleClickHandler }) {
+  const router = useRouter();
+  const title =
+    router.pathname in pages
+      ? pages[router.pathname]
+      : "Page not Found - tesKoran.id";
+
   return (
     <div className={styles.container}>
       <Script
@@ -18,12 +38,15 @@ export default function Layout({ children, hide, titleClickHandler }) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-         `}
+        `}
       </Script>
 
       <Head>
-        <title>tesKoran.id - Pauli & Kraepelin Test Online</title>
-        <meta name="description" content="Add them numbers" />
+        <title>{title}</title>
+        <meta
+          name="description"
+          content="Tes Koran Online, Pauli and Kraepelin"
+        />
         <meta name="viewport" content="width=device-width, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
